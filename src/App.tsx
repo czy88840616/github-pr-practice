d5eb42b0365464f8709572582e3f968c9a10c9c3
chenzhongyin.912 <chenzhongyin.912@bytedance.com>
2025-05-06 13:49:12 +0800

    chore: update base dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+import { createContext, useState } from "react";
 
-function App(): React.ReactElement {
-  const [count, setCount] = useState<number>(0)
+export const AuthContext = createContext({
+  isAuthenticated: false,
+  setIsAuthenticated: (value: boolean) => {},
+  logout: () => {},
+});
+
+export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AuthContext.Provider
+      value={{ isAuthenticated, setIsAuthenticated, logout }}
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
+      </Routes>
+    </AuthContext.Provider>
+  );
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from 'sonner';
+import App from "./App.tsx";
+import "./index.css";
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Failed to find the root element')
-
-createRoot(rootElement).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </StrictMode>
+);
